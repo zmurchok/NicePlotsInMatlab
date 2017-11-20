513e9af48218e26cc7a40998d8eca155c6ccb1c6
Cole Zmurchok <czmurchok@gmail.com>
2017-11-20 15:52:07 -0800

    subplot support

--- niceplots.m
+++ niceplots.m
@@ -1,4 +1,4 @@
-fact = 0.75;
+fact = 1;
 width=4*fact;
 height=3*fact;
 x0 = 5;
@@ -8,6 +8,21 @@ fontsize = 12;
 figure('Units','inches','Position',[x0 y0 width height],'PaperPositionMode','auto');
 
 x = 0:0.01:1;
+subplot(1,2,2)
+plot(x,x.^2,'k-',x,x.^3,'k-.','LineWidth',2)
+
+xlabel({'$x$'},'FontUnits','points','Interpreter','latex','FontWeight','normal','FontSize',fontsize,'FontName','Times')
+
+ylabel({'$y$'},'FontUnits','points','Interpreter','latex','FontWeight','normal','FontSize',fontsize,'FontName','Times')
+
+set(gca,'Units','normalized','FontUnits','points','FontWeight','normal','FontSize',fontsize,'FontName','Times')
+
+legend({'$x^2$','$x^3$'},'FontUnits','points', 'Interpreter','latex','FontSize',fontsize,'FontName','Times','Location','northeast')
+
+grid
+
+x = 0:0.01:1;
+subplot(1,2,1)
 plot(x,x.^2,'k-',x,x.^3,'k-.','LineWidth',2)
 
 xlabel({'$x$'},'FontUnits','points','Interpreter','latex','FontWeight','normal','FontSize',fontsize,'FontName','Times')
